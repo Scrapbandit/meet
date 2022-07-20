d6aa95ded5ba06a1d8fb2ed742a19ba802ec79d1
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2022-07-20 12:30:25 +0200

    add responsivness to the chaart

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Event from './Event';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import WelcomeScreen from './WelcomeScreen';
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 class App extends Component {
   state = {
@@ -86,19 +86,17 @@ class App extends Component {
           updateNumberOfEvents={this.updateNumberOfEvents}
         />
         <h4>Events in each city</h4>
-        <ScatterChart
-          width={800}
-          height={400}
-          margin={{
-            top: 20, right: 20, bottom: 20, left: 20,
-          }}
-        >
+
+        <ResponsiveContainer height={400} >
+         <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20, }}>
           <CartesianGrid />
           <XAxis type="category" dataKey="city" name="city" />
-          <YAxis type="number" dataKey="number" name="number of events" />
-          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <YAxis allowDecimals={false}
+           type="number" dataKey="number" name="number of events" />
+           <Tooltip cursor={{ strokeDasharray: '3 3' }} />
           <Scatter data={this.getData()} fill="#8884d8" />
         </ScatterChart>
+       </ResponsiveContainer>
 
         <EventList
           events={this.state.events}
