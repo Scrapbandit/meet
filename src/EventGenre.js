d435ecc9ecc15e87cba342b34c55c31f3be29117
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2022-07-20 15:36:18 +0200

    fix last errors event genre

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -3,20 +3,19 @@ import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const EventGenre = ({ locations ,events }) => {
  const [data, setData] = useState([]);
- const colors = ['#8884d8', '#83a6ed', '#8dd1e1', '#82ca9d', '#a4de6c', '#d0ed57'];
+ const colors = ['#8884d8'];
  useEffect(() => { setData(() => getData());}, [locations, events]);
 
  const getData = () => {
     const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
      const data = genres.map((genre)=>{
       const value = events.filter((event) =>
-        event.summary.split(" ").includes(genre)
+        event.summary.split(' ').includes(genre)
       ).length;
-      return { name: genre, value: value };
+      return { name: genre, value };
     });
     return data;
   }
- 
 
 return (
     <ResponsiveContainer height={400}>
