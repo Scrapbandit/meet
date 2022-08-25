c93ed63a3a1604132d74afdfa7ce9eb0de97bb91
Scrapbandit <99894181+Scrapbandit@users.noreply.github.com>
2022-08-25 13:57:17 +0200

    rendering fix

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,44 +1,89 @@
-import React, { useEffect, useState } from "react";
-import { PieChart, Pie, ResponsiveContainer } from "recharts";
+import React, { useEffect, useState } from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
-const EventGenre = ({ events }) => {
-  const [data, setData] = useState([]);
+const EventGenre = ({ locations ,events }) => {
+  console.log("Event Genere", locations, events)
+ const [data, setData] = useState([]);
+ const colors = ['#8884d8'];
+ useEffect(() => { setData(() => { getData(); console.log("Data for Pie", data) });}, [locations, events]);
 
-  const getData = () => {
-    const genres = ["React", "Javascript", "Node", "jQuery", "AngularJS"];
-    const data = genres.map((genre) => {
-      const value = events.filter(({ summary }) =>
-        summary.replace(".js", "").split(" ").includes("genre")
+ const getData = () => {
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+     const data = genres.map((genre)=>{
+      console.log("EVENTS", events)
+      const value = events.filter((event) =>
+        event.summary.split(' ').includes(genre)
       ).length;
-
       return { name: genre, value };
     });
     return data;
-  };
+  }
 
-  useEffect(() => {
-    setData(() => getData());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [events]);
-
-  return (
+return (
     <ResponsiveContainer height={400}>
       <PieChart width={400} height={400}>
         <Pie
-          data={data}
-          cx={200}
-          cy={200}
-          labelLine={false}
-          outerRadius={80}
-          fill="#8884d8"
-          dataKey="value"
-          label={({ name, percent }) =>
-            `${name} ${(percent * 100).toFixed(0)}%`
-          }
-        ></Pie>
-      </PieChart>
+            data={data}
+            cx={200}
+            cy={200}
+            labelLine={false}
+            outerRadius={80}
+            fill='#8884d8'
+            dataKey='value'
+            label={({ name, percent }) => `${name} ${( percent * 100).toFixed (0)}%`}>
+            {data.map((entry, index) => <Cell key={`cell-${index}`} fill={colors[index]} name={entry.name}/>)}
+
+            </Pie>
+        </PieChart>
     </ResponsiveContainer>
   );
-};
+}
+
 
 export default EventGenre;
+
+
+// import React, { useEffect, useState } from "react";
+// import { PieChart, Pie, ResponsiveContainer } from "recharts";
+
+// const EventGenre = ({ events }) => {
+//   const [data, setData] = useState([]);
+
+//   const getData = () => {
+//     const genres = ["React", "Javascript", "Node", "jQuery", "AngularJS"];
+//     const data = genres.map((genre) => {
+//       const value = events.filter(({ summary }) =>
+//         summary.replace(".js", "").split(" ").includes("genre")
+//       ).length;
+
+//       return { name: genre, value };
+//     });
+//     return data;
+//   };
+
+//   useEffect(() => {
+//     setData(() => getData());
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [events]);
+
+//   return (
+//     <ResponsiveContainer height={400}>
+//       <PieChart width={400} height={400}>
+//         <Pie
+//           data={data}
+//           cx={200}
+//           cy={200}
+//           labelLine={false}
+//           outerRadius={80}
+//           fill="#8884d8"
+//           dataKey="value"
+//           label={({ name, percent }) =>
+//             `${name} ${(percent * 100).toFixed(0)}%`
+//           }
+//         ></Pie>
+//       </PieChart>
+//     </ResponsiveContainer>
+//   );
+// };
+
+// export default EventGenre;
